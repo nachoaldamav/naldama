ac1f4279903ccf0078d62351e4cf8789028790f2
srdrabx <devalladolidoficial@gmail.com>
2022-08-05 09:23:48 +0200

    Add entry file to tailwind

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "../../node_modules/ui/**/*.{js,ts,jsx,tsx}",
+    "../../node_modules/ui/dist/index.js",
   ],
   theme: {
     extend: {},
